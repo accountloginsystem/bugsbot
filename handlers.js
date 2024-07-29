66db60ab7624a6782e9c0293c05c7c693adbd513
accountloginsystem <168574688+accountloginsystem@users.noreply.github.com>
2024-07-29 17:44:56 +0530

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -1,19 +1,14 @@
-const { Telegraf, Markup } = require('telegraf');
-const { WEB_APP_URL, TELEGRAM_BOT_TOKEN } = require('./config');
+const { Markup } = require('telegraf');
+const { WEB_APP_URL } = require('./config');
 const fs = require('fs');
 
-// Initialize the bot with your token
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
-
-// Handler for the /start command
 const startHandler = async (ctx) => {
   const chatId = ctx.message.chat.id;
-  const messageText = ctx.message.text;
-  const referralId = messageText.includes('=') ? messageText.split('=')[1] : '';
-  const username = ctx.message.chat.username || 'User';
+  const referralId = ctx.message.text.split('=')[1] || '';
+  const username = ctx.message.chat.username;
 
   // Personalized welcome message
-  const welcomeMessage = `${username}, Welcome to HodlSwap!`;
+  const welcomeMessage = `${username} Welcome to HodlSwap!`;
   const description = `HodlSwap is like a treasure hunt for tokens! Users can earn them by using different mining app features. And guess what? The players get most of the tokens!
 
 Let's gather your squad! More buddies mean more coins.
@@ -23,11 +18,6 @@ Let's make it rain!`;
   // Use a local image file
   const imagePath = 'hodlswap_image.png';
 
-  // Construct the URL to send to the web app
-  const webAppUrl = referralId 
-    ? `${WEB_APP_URL}/home/${chatId}/${referralId}` 
-    : `${WEB_APP_URL}/home/${chatId}`;
-
   // Send the welcome message with image, text, and buttons
   await ctx.replyWithPhoto(
     { source: imagePath },
@@ -35,7 +25,7 @@ Let's make it rain!`;
       caption: `${welcomeMessage}\n\n${description}`,
       reply_markup: {
         inline_keyboard: [
-          [{ text: 'Play', web_app: { url: webAppUrl } }],
+          [{ text: 'Play', web_app: { url: referralId ? `${WEB_APP_URL}/home/${chatId}/${referralId}` : `${WEB_APP_URL}/home/${chatId}` } }],
           [{ text: 'Join Our Channel', url: 'https://t.me/HodL_Swap' }],
           // [{ text: 'Help', callback_data: '/help' }]
         ]
@@ -44,9 +34,9 @@ Let's make it rain!`;
   );
 };
 
-// Handler for the /help command
 const helpHandler = async (ctx) => {
   const chatId = ctx.message.chat.id;
+  const referralId = ctx.message.text.split('=')[1] || '';
 
   await ctx.reply('To use this bot, please start by sending the /start command. This will take you to the HodlSwap platform where you can start earning tokens!', 
     Markup.inlineKeyboard([
@@ -55,23 +45,13 @@ const helpHandler = async (ctx) => {
   );
 };
 
-// Handler for data received from the web app
 const webAppDataHandler = async (ctx) => {
+  // Handle data received from the web app
   console.log('Web App Data:', ctx.webAppData);
 };
 
-// Register handlers
-bot.start(startHandler);
-bot.help(helpHandler);
-bot.on('web_app_data', webAppDataHandler);
-
-// Start the bot
-bot.launch().then(() => {
-  console.log('Bot started successfully');
-}).catch((err) => {
-  console.error('Failed to start the bot:', err);
-});
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+module.exports = {
+  startHandler,
+  helpHandler,
+  webAppDataHandler
+};
