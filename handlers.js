6c7464cc897dc551e6158929ee9498ecd8b1a370
accountloginsystem <168574688+accountloginsystem@users.noreply.github.com>
2024-09-15 16:43:56 +0530

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -15,30 +15,32 @@ const startHandler = async (ctx) => {
   }
 
   console.log(ctx.message);
-  console.log("refid"+referralId);
-  
-  
+  console.log("refid" + referralId);
+
   // Personalized welcome message
-  const welcomeMessage = `${username} Welcome to HodlSwap!`;
-  const description = `HodlSwap is like a treasure hunt for tokens! Users can earn them by using different mining app features. And guess what? The players get most of the tokens!
+  const welcomeMessage = `${username}, Welcome to Bugs!`;
+  const description = `Bugs is like a treasure hunt for tokens! Users can earn them by using different app features. And guess what? The players get most of the tokens!
 
 Let's gather your squad! More buddies mean more coins.
 
 Let's make it rain!`;
 
-  // Use a local image file
-  const imagePath = 'hodlswap_image.png';
+  // Message for the $50 reward
+  const rewardMessage = `🎉 Special Offer: Get a $50 reward for participating in our Bugs bounty program! Complete tasks, invite friends, and be rewarded. Claim your $50 now! 🎉`;
 
-  // Send the welcome message with image, text, and buttons
+  // Use a local image file
+  const imagePath = 'bugs_image.png';
+
+  // Send the welcome message with image, description, reward, and buttons
   await ctx.replyWithPhoto(
     { source: imagePath },
     {
-      caption: `${welcomeMessage}\n\n${description}`,
+      caption: `${welcomeMessage}\n\n${description}\n\n${rewardMessage}`,
       reply_markup: {
         inline_keyboard: [
           [{ text: 'Play', web_app: { url: referralId ? `${WEB_APP_URL}/home/${chatId}/${referralId}` : `${WEB_APP_URL}/home/${chatId}` } }],
-          [{ text: 'Join Our Channel', url: 'https://t.me/hodlswap' }],
-          // [{ text: 'Help', callback_data: '/help' }]
+          [{ text: 'Join Our Channel', url: 'https://t.me/bugs' }],
+          // Add more buttons if needed
         ]
       }
     }
@@ -49,7 +51,7 @@ const helpHandler = async (ctx) => {
   const chatId = ctx.message.chat.id;
   const referralId = ctx.message.text.split('=')[1] || '';
 
-  await ctx.reply('To use this bot, please start by sending the /start command. This will take you to the HodlSwap platform where you can start earning tokens!', 
+  await ctx.reply('To use this bot, please start by sending the /start command. This will take you to the Bugs platform where you can start earning tokens!', 
     Markup.inlineKeyboard([
       [{ text: 'Start', callback_data: '/start' }]
     ])
