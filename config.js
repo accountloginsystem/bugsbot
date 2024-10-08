ba9150b5e813cf77c7817b83d8981dd9d355e576
accountloginsystem <168574688+accountloginsystem@users.noreply.github.com>
2024-10-08 17:38:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 module.exports = {
   BOT_TOKEN: '7320398607:AAE2icvBKgC733LcGIEMJZTh1hYxV3fOt-o',
-  WEB_APP_URL: 'https://coral-app-avvyd.ondigitalocean.app'
+  WEB_APP_URL: 'https://bugs-3e2fb.web.app/'
 };
